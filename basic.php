2ec580c0c2ca939f673be71768aa143120e10e15
harishvarmaj <122346805+harishvarmaj@users.noreply.github.com>
2024-08-27 20:50:19 +0530

    Add files via upload

---
+++ basic.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+ 
+<?php
+
+    $name='Varma';
+   // $num= ' 143';
+    echo   "<h1>$name  king of koo</h1>";
+?>
+
+</body>
+</html>
---
+++ joining .php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+ 
+<?php
+
+    $name1 = 'Harish';
+    $name2 = 'Varma';
+    $names= $name1.$name2;
+    echo $names;
+?>
+
+</body>
+</html>
